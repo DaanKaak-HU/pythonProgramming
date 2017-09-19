0fd64b96158b5edb6c65f6249946e0ca653f3e64
Daan <daan.kaak@student.hu.nl>
2017-09-19 14:14:18 +0200

    test

--- Les5/pe5_1.py
+++ Les5/pe5_1.py
@@ -1,10 +1,3 @@
-import unittest
-
-
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
-
-
-if __name__ == '__main__':
-    unittest.main()
+text = 'ADF'
+result = text.lower()
+print(result)
